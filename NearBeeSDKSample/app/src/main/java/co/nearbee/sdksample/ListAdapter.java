1755a7a93c2e86a6434cefa3174afa7f8c70469c
Kislay <kislay@mobstac.com>
2018-11-02 14:37:31 +0530

    Add Ui to show active beacons

---
+++ NearBeeSDKSample/app/src/main/java/co/nearbee/sdksample/ListAdapter.java
@@ -0,0 +1,82 @@
+package co.nearbee.sdksample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import co.nearbee.NearBeeBeacon;
+
+/**
+ * Copyright (C) 2018 Mobstac, Inc.
+ * All rights reserved
+ *
+ * @author Kislay
+ * @since 02/11/18
+ */
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.BeaconViewHolder> {
+
+    private ArrayList<NearBeeBeacon> beacons;
+
+    ListAdapter(ArrayList<NearBeeBeacon> beacons) {
+        this.beacons = beacons;
+    }
+
+    @NonNull
+    @Override
+    public BeaconViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(
+                R.layout.list_item_beacon,
+                viewGroup,
+                false
+        );
+        return new BeaconViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final BeaconViewHolder viewHolder, int i) {
+        final NearBeeBeacon beacon = beacons.get(i);
+        viewHolder.title.setText(beacon.getNotification().getTitle());
+        viewHolder.description.setText(beacon.getNotification().getDescription());
+        viewHolder.link.setText(beacon.getNotification().getEddystoneURL());
+        Glide.with(viewHolder.icon.getContext())
+                .load(beacon.getNotification().getIcon())
+                .into(viewHolder.icon);
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(beacon.getNotification().getDescription()));
+                viewHolder.icon.getContext().startActivity(i);
+            }
+        });
+    }
+
+    class BeaconViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView title, description, link;
+        private AppCompatImageView icon;
+
+        public BeaconViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            link = itemView.findViewById(R.id.link);
+            icon = itemView.findViewById(R.id.icon);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return beacons.size();
+    }
+}
--- NearBeeSDKSample/app/src/main/java/co/nearbee/sdksample/MainActivity.java
+++ NearBeeSDKSample/app/src/main/java/co/nearbee/sdksample/MainActivity.java
@@ -2,12 +2,89 @@ package co.nearbee.sdksample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import co.nearbee.NearBee;
+import co.nearbee.NearBeeBeacon;
+import co.nearbee.NearBeeException;
+import co.nearbee.NearBeeListener;
+
+public class MainActivity extends AppCompatActivity implements NearBeeListener {
+
+    NearBee nearBee;
+    ListAdapter adapter;
+    RecyclerView recyclerView;
+    ArrayList<NearBeeBeacon> beacons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.beacon_list);
+        nearBee = new NearBee.Builder(this)
+                .setBackgroundNotificationsEnabled(true)
+                .build();
+        nearBee.startScanning(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (nearBee != null && !nearBee.isScanning())
+            nearBee.startScanning(this);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (nearBee != null)
+            nearBee.stopScanning();
+    }
+
+    @Override
+    public void onUpdate(ArrayList<NearBeeBeacon> beaconsInRange) {
+        Log.e("sdk", "Found " + beaconsInRange.size() + " beacons");
+        if (adapter == null) {
+            this.beacons = new ArrayList<>(beaconsInRange);
+            adapter = new ListAdapter(this.beacons);
+            LinearLayoutManager manager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(manager);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onBeaconLost(ArrayList<NearBeeBeacon> lost) {
+        Log.e("sdk", "Lost " + lost.size() + " beacons");
+        if (adapter != null) {
+            for (NearBeeBeacon beacon : lost) {
+                beacons.remove(beacon);
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onBeaconFound(ArrayList<NearBeeBeacon> found) {
+        for (NearBeeBeacon beacon : found) {
+            Log.e("sdk", "Found " + beacon.getNotification().getTitle());
+        }
+        if (adapter != null) {
+            for (NearBeeBeacon beacon : found) {
+                if (!beacons.contains(beacon))
+                    beacons.add(beacon);
+            }
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onError(NearBeeException exception) {
+        Log.e("sdk", "Error: " + exception.getMessage());
     }
 }
